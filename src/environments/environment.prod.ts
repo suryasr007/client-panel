5a4e28290471a0b1abecb4beaf00e5cc1715fd8a
suryasr007 <94suryateja@gmail.com>
2018-05-01 23:56:53 +0530

    firestore integration complete

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "xxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "clientplanelprod.firebaseapp.com",
+    databaseURL: "https://clientplanelprod.firebaseio.com",
+    projectId: "clientplanelprod",
+    storageBucket: "clientplanelprod.appspot.com",
+    messagingSenderId: "xxxxxxxxxxxxxxxx"
+  }
+};
